92733dae657da84395865281d30ceb860969fd0b
Suprim Rana Magar <251268805+Suprim-Rana-Magar@users.noreply.github.com>
2026-01-08 07:35:47 +0545

    greatest using function

---
+++ greatestfunction.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int greatest(int,int,int);
+int main() {
+    int a,b,c,d;
+    printf("Enter 1st number:\n");
+    scanf("%d",&a);
+    printf("Enter 2nd number:\n");
+    scanf("%d",&b);
+    printf("Enter 3rd number:\n");
+    scanf("%d",&c);
+    d=greatest(a,b,c);
+    printf("The greatest number is %d.",d);
+    return 0;
+}
+int greatest(int a,int b,int c) {
+    if(a>b&&a>c) {
+        return a;
+    }
+    else if(b>c&&b>a) {
+        return b;
+    }
+    else {
+        return c;
+    }
+}
