644853d89466dd5ae481b32084c2d0f97afcab45
Suprim Rana Magar <251268805+Suprim-Rana-Magar@users.noreply.github.com>
2026-01-22 07:31:33 +0545

    swap call by value

---
+++ recursivefunction5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void swap(int,int);
+int main() {
+    int a,b;
+    a=99;b=89;
+    printf("\n Before function calling,a and b:%d\t%d",a,b);
+    swap(a,b);
+    printf("\n After function calling,a and b are:%d\t%d",a,b);
+    return 0;
+}
+void swap(int x,int y) {
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+    printf("\n The value within function are:%d\t%d",x,y);
+}
